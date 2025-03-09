47157f98d8c235cd7b6b4261836041e7096add89
laopo001 <0laopo0@gmail.com>
2025-03-09 20:50:45 +0800

    Handle auto-launcher initialization error and update form field values in settings

--- web/src/pages/setting/index.tsx
+++ web/src/pages/setting/index.tsx
@@ -65,7 +65,9 @@ export function Setting() {
     (async () => {
       await AppSetting.init();
       await electronData.init();
-      AppSetting.get().isAutoLauncher = await call("isAutoLauncher"); // 获取是否自动启动
+      AppSetting.get().isAutoLauncher = await call("isAutoLauncher").catch(
+        (x) => AppSetting.get().isAutoLauncher,
+      ); // 获取是否自动启动
       webdavForm.resetFields();
       webdavForm.setFieldsValue(
         Object.assign(AppSetting.get().webdav, { baseDirName: "HyperChat" }),
@@ -203,14 +205,15 @@ export function Setting() {
             name="basicSitting"
             labelCol={{ span: 8 }}
             wrapperCol={{ span: 16 }}
-            initialValues={{
-              isAutoLauncher: AppSetting.get().isAutoLauncher,
-              mcpCallToolTimeout: AppSetting.get().mcpCallToolTimeout,
-            }}
+            // initialValues={{
+            //   isAutoLauncher: AppSetting.get().isAutoLauncher,
+            //   mcpCallToolTimeout: AppSetting.get().mcpCallToolTimeout,
+            // }}
             autoComplete="off"
           >
-            <Form.Item label={t`LaunchStartup`} name="isAutoLauncher">
+            <Form.Item label={t`LaunchStartup`}>
               <Switch
+                value={AppSetting.get().isAutoLauncher}
                 checkedChildren="Startup"
                 unCheckedChildren="Close"
                 onChange={async (value) => {
@@ -225,10 +228,12 @@ export function Setting() {
                 }}
               ></Switch>
             </Form.Item>
-            <Form.Item label={t`mcpCallToolTimeout`} name="mcpCallToolTimeout">
+            <Form.Item label={t`mcpCallToolTimeout`}>
               <InputNumber
+                value={AppSetting.get().mcpCallToolTimeout}
                 onChange={async (value) => {
-                  AppSetting.get().mcpCallToolTimeout = parseInt(value as any) || 60;
+                  AppSetting.get().mcpCallToolTimeout =
+                    parseInt(value as any) || 60;
                   await AppSetting.save();
                   refresh();
                 }}
