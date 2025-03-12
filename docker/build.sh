90be96b86a01281538ed9e1e4414177a89ffc5e9
laopo001 <0laopo0@gmail.com>
2025-03-12 16:35:56 +0800

    Update Dockerfile and build script for HyperChat with new startup commands and port mappings

--- docker/build.sh
+++ docker/build.sh
@@ -3,6 +3,6 @@ docker build -t hyperchat .
 docker rm hyperchat && docker run --name hyperchat -p 6080:80 -v /dev/shm:/dev/shm hyperchat
 
 
-docker rm hyperchat && docker run --name hyperchat -p 5901:5901 -p 6901:6901 -e VNC_PASSWORDLESS=true hyperchat
+docker rm hyperchat && docker run --name hyperchat -p 5901:5901 -p 6901:6901 -p 16200:16100 -v C:\\Users\\0laop\\Documents\\HyperChat:~/Documents/HyperChat -e VNC_PASSWORDLESS=true hyperchat
 
-docker run -it --user 0 -p 6911:6901 consol/debian-xfce-vnc
+# docker run -it --user 0 -p 6911:6901 consol/debian-xfce-vnc
