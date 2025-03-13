efd7740d6262833fde5bd5cfa71f42327253236e
sasakiyori <sasakiyori33@gmail.com>
2025-03-13 17:29:18 +0800

    Update dockerfile & build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,7 @@
-docker build -t hyperchat .
+RELEASE=1.1.1
+ARCH=linux-x64
 
-docker rm hyperchat && docker run --name hyperchat -p 6080:80 -v /dev/shm:/dev/shm hyperchat
-
-
-docker rm hyperchat && docker run --name hyperchat -p 5901:5901 -p 6901:6901 -p 16200:16100 -v C:\\Users\\0laop\\Documents\\HyperChat:~/Documents/HyperChat -e VNC_PASSWORDLESS=true hyperchat
-
-# docker run -it --user 0 -p 6911:6901 consol/debian-xfce-vnc
+docker build \
+    --build-arg RELEASE=${RELEASE} \
+    --build-arg ARCH=${ARCH} \
+    -t hyperchat:${RELEASE} .
