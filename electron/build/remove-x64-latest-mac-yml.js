9afdf4d31f755c170e484658f63e6e8132efad14
Your Name <your.email@example.com>
2025-04-16 04:02:09 +0800

    refactor: 将最新的 mac 配置文件删除逻辑封装到 afterAllArtifactBuild 函数中

--- electron/build/remove-x64-latest-mac-yml.js
+++ electron/build/remove-x64-latest-mac-yml.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-const file = path.join(__dirname, '..', 'dist', 'latest-mac.yml');
-if (fs.existsSync(file)) {
-    fs.rmSync(file);
-    console.log('latest-mac.yml removed before publish.');
+
+// scripts/afterAllArtifactBuild.js
+exports.default = async function afterAllArtifactBuild(context) {
+    const file = path.join(__dirname, '..', 'dist', 'latest-mac.yml');
+    if (fs.existsSync(file)) {
+        fs.rmSync(file);
+        console.log('latest-mac.yml removed before publish.');
+    }
 }
